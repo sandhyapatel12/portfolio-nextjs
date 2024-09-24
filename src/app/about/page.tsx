b156b6369c975e331fb40bb03c2bf1a5f61a0995
sandhyapatel12 <sandhyapt30@gmail.com>
2024-09-24 18:02:44 +0530

    'update"

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,6 +13,8 @@ const page = () => {
         </div>
 
         <div className='max-w-5xl mx-auto w-full  rounded-lg md:flex md:justify-between px-5  '>
+
+          {/* left side */}
           <div className='py-5 flex flex-col px-10  justify-center '>
             <h1 className='text-2xl md:text-3xl lg:4xl font-bold text-green-600'>I'm Frontend Developer</h1>
             <p className='text-gray-400 mt-8 leading-7'>I am a passionate frontend developer with a knack for turning complex ideas into visually appealing and user-friendly web interfaces. With expertise in HTML5, CSS3, JavaScript, and modern frontend frameworks like react js.</p>
@@ -21,6 +23,7 @@ const page = () => {
             </Link>
           </div>
 
+          {/* right side */}
           <div className='w-full'>
             <Image src="https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
               alt='img'
@@ -28,6 +31,7 @@ const page = () => {
               width={350}
               className='rounded-full  px-5 py-5' />
           </div>
+
         </div>
       </section>
     </>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
         <>
             <section className='min-h-screen flex items-center justify-center'>
 
-                <div className="mt-20">
+                <div className="mt-24">
 
                     <div className='max-w-5xl mx-auto  flex px-10 space-x-3 '>
                         <div className='bg-green-600 h-8 w-2 rounded-md'></div>
@@ -19,99 +19,113 @@ export default function Projects() {
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
 
                             {/* portfolio website */}
-                            <div className="max-w-xs w-full">
+                            <div className="max-w-xs w-full border border-gray-400 rounded-lg p-3">
                                 <div
                                     className={cn(
-                                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-                                        "bg-[url('/projects/p1.png')] bg-contain bg-no-repeat	",
+                                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border  border-transparent dark:border-neutral-800",
+                                        "bg-[url('/projects/p1.png')] bg-cover	",
                                     )}>
+
                                 </div>
 
+                                <h1 className=" text-gray-400 font-bold mt-3"> I developed a personal portfolio website to showcase my skills, projects, and experience.</h1>
+
                                 <div className="flex justify-center items-center mt-3 space-x-8">
-                                    <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">view</button>
-                                    <Link href='https://github.com/sandhyapatel12/portfolio-nextjs'>
-                                        <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">code</button>
+                                <button className="py-2 px-8 rounded-full border-2 border-gray-500 text-green-600 font-bold hover:text-white">view</button>
+                                <Link href='https://github.com/sandhyapatel12/portfolio-nextjs'>
+                                    <button className="py-2 px-8 rounded-full border-2 border-gray-500 text-green-600 font-bold hover:text-white">code</button>
                                     </Link>
                                 </div>
+
+
                             </div>
 
                             {/* social media app */}
-                            <div className="max-w-xs w-full">
+                            <div className="max-w-xs w-full border border-gray-400 rounded-lg p-3">
                                 <div
                                     className={cn(
                                         "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-                                        "bg-[url('/projects/p2.png')]  bg-contain bg-no-repeat",
+                                        "bg-[url('/projects/p2.png')]  bg-cover ",
                                     )}>
                                 </div>
+
+                                <h1 className=" text-gray-400 font-bold mt-3"> This is social media app which is developed in react js with all social funcnality.</h1>
+
                                 <div className="flex justify-center items-center mt-3 space-x-8">
-                                    <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">view</button>
                                     <Link href='/'>
-                                        <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">code</button>
+                                        <button className="py-2 px-8 rounded-full border-2 border-gray-500 text-green-600 font-bold hover:text-white">code</button>
                                     </Link>
                                 </div>
                             </div>
 
                             {/* food ordering system */}
-                            <div className="max-w-xs w-full">
+                            <div className="max-w-xs w-full border border-gray-400 rounded-lg p-3">
                                 <div
                                     className={cn(
                                         "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-                                        "bg-[url('/projects/p3.png')]  bg-contain bg-no-repeat",
+                                        "bg-[url('/projects/p3.png')]  bg-cover",
                                     )}>
-
                                 </div>
+
+                                <h1 className=" text-gray-400 font-bold mt-3"> In this app includes payment gatway, login with google, register user with backend.</h1>
+
                                 <div className="flex justify-center items-center mt-3 space-x-8">
-                                    <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">view</button>
                                     <Link href='https://github.com/sandhyapatel12/food-ordering-System-redux-toolk'>
-                                        <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">code</button>
+                                        <button className="py-2 px-8 rounded-full border-2 border-gray-500 text-green-600 font-bold hover:text-white">code</button>
                                     </Link>
                                 </div>
                             </div>
 
                             {/* ecommerce app*/}
-                            <div className="max-w-xs w-full">
+                            <div className="max-w-xs w-full border border-gray-400 rounded-lg p-3">
                                 <div
                                     className={cn(
                                         "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-                                        "bg-[url('/projects/p4.png')] bg-contain bg-no-repeat",
+                                        "bg-[url('/projects/p4.png')] bg-cover",
                                     )} >
                                 </div>
 
+                                <h1 className=" text-gray-400 font-bold mt-3"> In this ecommerce website user can find product by serching or also use filter funcnality.</h1>
+
                                 <div className="flex justify-center items-center mt-3 space-x-8">
-                                    <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">view</button>
                                     <Link href='https://github.com/sandhyapatel12/ecommerce-website-react'>
-                                        <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">code</button>
+                                    <button className="py-2 px-8 rounded-full border-2 border-gray-500 text-green-600 font-bold hover:text-white">code</button>
                                     </Link>
                                 </div>
                             </div>
 
                             {/* inotebook */}
-                            <div className="max-w-xs w-full">
+                            <div className="max-w-xs w-full border border-gray-400 rounded-lg p-3">
                                 <div
                                     className={cn(
                                         "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-                                        "bg-[url('/projects/p5.png')]  bg-contain bg-no-repeat",
+                                        "bg-[url('/projects/p5.png')]  bg-cover ",
                                     )} >
                                 </div>
+
+                                <h1 className=" text-gray-400 font-bold mt-3">In this full stack app user can add their notes and view all own notes by valid login.</h1>
+
                                 <div className="flex justify-center items-center mt-3 space-x-8">
-                                    <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">view</button>
                                     <Link href='https://github.com/sandhyapatel12/iNotebook-react'>
-                                        <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">code</button>
+                                    <button className="py-2 px-8 rounded-full border-2 border-gray-500 text-green-600 font-bold hover:text-white">code</button>
                                     </Link>
                                 </div>
                             </div>
+
                             {/* textutils */}
-                            <div className="max-w-xs w-full">
+                            <div className="max-w-xs w-full border border-gray-400 rounded-lg p-3">
                                 <div
                                     className={cn(
                                         "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-                                        "bg-[url('/projects/p6.png')]  bg-contain bg-no-repeat",
+                                        "bg-[url('/projects/p6.png')]  bg-cover ",
                                     )}>
                                 </div>
+
+                                <h1 className=" text-gray-400 font-bold mt-3"> In this react app  user can convert any text into uppercase, lowercase, copy text etc.</h1>
+ 
                                 <div className="flex justify-center items-center mt-3 space-x-8">
-                                    <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">view</button>
                                     <Link href='https://github.com/sandhyapatel12/TextUtils-react'>
-                                        <button className="py-2 px-8 rounded-lg bg-green-600 text-black font-bold hover:text-white">code</button>
+                                    <button className="py-2 px-8 rounded-full border-2 border-gray-500 text-green-600 font-bold hover:text-white">code</button>
                                     </Link>
                                 </div>
                             </div>
